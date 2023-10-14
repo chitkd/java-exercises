fa9aa8744746d651f064e1623c25abbc2631d312
KimChi2893 <dtkchi2893@gmail.com>
2023-10-14 21:16:14 +0700

    resubmit lab 7.2 + rearrange package lesson_8

--- src/exercises/lesson_8/TestEncapsulation.java
+++
@@ -1,8 +0,0 @@
-package exercises.lesson_8;
-
-public class TestEncapsulation {
-    public static void main(String[] args) {
-        Person teo = new Person("teo", -18);
-        System.out.printf("My name is: %s\n and I'm %s years old", teo.getName(),  teo.getAge());
-    }
-}
